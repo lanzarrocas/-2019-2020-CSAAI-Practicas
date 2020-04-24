07433537b67bcecabb05f8327cd963fe908e1e7f
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-04-24 20:29:17 +0200

    Sesión Laboratorio 7: Práctica 3-1 (3)
    Añado movimiento de la pelota

--- P3/pong-01.js
+++ P3/pong-01.js
@@ -2,6 +2,8 @@ console.log("Ejecutando JS...");
 
 //-- Obtener el objeto canvas
 const canvas = document.getElementById("canvas");
+//-- Variable para la posicion de la bola (en x)
+let bola_x = canvas.height/4;
 
 //-- Sus dimensiones las hemos fijado en el fichero
 //-- HTML. Las imprimimos en la consola
@@ -10,6 +12,8 @@ console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 //-- Obtener el contexto para pintar en el canvas
 const ctx = canvas.getContext("2d");
 
+//-- Pintar todos los objetos en el canvas
+function draw() {
         //----- Dibujar la Bola
         ctx.beginPath();
         //----- Propiedades pelota
@@ -21,7 +25,7 @@ const ctx = canvas.getContext("2d");
         ctx.strokeStyle = "#006400";
         ctx.fillStyle = "#6ab150";
         ctx.lineWidth = 5;
-        ctx.arc(X,Y,r,0,2*Math.PI);
+        ctx.arc(bola_x,Y,r,0,2*Math.PI);
         ctx.fill();
         ctx.stroke();
 
@@ -66,3 +70,43 @@ const ctx = canvas.getContext("2d");
         //-- Dibujar hasta el punto inferior
         ctx.lineTo(canvas.width/2, canvas.height);
         ctx.stroke();
+
+        //------ Dibujar el tanteo
+        ctx.font = "100px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("0", canvas.width/3, 80);
+        ctx.fillText("1", canvas.width/(9/5), 80);
+}
+
+//---- Bucle principal de la animación
+function animacion() {
+      //-- Actualizar las posiciones de los objetos móviles
+      //-- De momento no lo estamos haciendo
+
+      //-- Borrar la pantalla
+      ctx.clearRect(0,0, canvas.width, canvas.height);
+
+      //-- Dibujar el nuevo frame
+      draw();
+      //-- Mostrar actividad en la consola
+      //-- Para comprobar que "está vivo",
+      //-- aunque no se mueva nada en la pantalla
+      //-- todavía
+      console.log("Frame!");
+    }
+
+//-- Arrancar la animación
+setInterval(()=>{
+  animacion();
+},16);
+
+//-- Obtener el boton de dar un "paso"
+const paso = document.getElementById("paso");
+
+//-- Botón de dar un Paso: Cada vez que lo apretamos
+//-- la bola avanza 5 píxeles
+paso.onclick = () => {
+  //-- Incrementar la posicion x de la bola
+  bola_x += 5;
+  console.log("Paso!");
+}
