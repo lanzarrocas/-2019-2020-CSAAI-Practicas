445e74f1bee414e741f048bf7d36b03d9d883516
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-13 03:06:23 +0200

    (4)

--- P4/video-02.js
+++ P4/video-02.js
@@ -2,10 +2,6 @@ console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
 const mainv = document.getElementById('mainv')
-const v1 = document.getElementById('v1')
-const v2 = document.getElementById('v2')
-const v3 = document.getElementById('v3')
-
 
 mainv.width=600;  //-- Tamaño de la pantalla de video main
 mainv.height=300;
@@ -18,25 +14,19 @@ for (var i = 0; i < videos.length; i++) {
 
 
 function main (video) {
-  console.log(video.id);
-  var main = document.getElementById("mainv");
-  video.style = "border: 10px solid #00FF00; width: 22.5%";
-  switch (video.id) {
-    case "v1":
-        v2.style = "border: 10px solid black; width: 22.5%";
-        v3.style = "border: 10px solid black; width: 22.5%";
-      break;
-    case "v2":
-        v1.style = "border: 10px solid black; width: 22.5%";
-        v3.style = "border: 10px solid black; width: 22.5%";
-      break;
-    case "v3":
-        v1.style = "border: 10px solid black; width: 22.5%";
-        v2.style = "border: 10px solid black; width: 22.5%";
-      break;
-    default:
+
+  for (var i = 0; i < videos.length; i++) {
+    if (video.id == videos[i].id) {
+      video.style = "border: 10px solid #00FF00; width: 22.5%";
+    } else {
+      videos[i].style = "border: 10px solid black; width: 22.5%";
     }
+  }
     mainv.src = video.src;
-    main.currentTime = video.currentTime;
+    mainv.currentTime = video.currentTime;
     mainv.play();
-}
+    for (var i = 0; i < videos.length; i++) {
+      videos[i].play();
+      videos[i].muted=true;
+    }
+  }
