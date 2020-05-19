8f3a8dad9aacf229dba91cf3c7f5d9323e8079c2
lanzarrocas <56543649+lanzarrocas@users.noreply.github.com>
2020-05-19 03:53:12 +0200

    L10: Práctica-4-1 (6)
    MEJORA AÑADIDA
    Imagen estática: Añadir un cuarto canal que permita mostrar una imagen estática, con un mensaje que ponga "Emisión en Pruebas". Este canal se podrá seleccionar en cualquier momento

--- P4/video-02.js
+++ P4/video-02.js
@@ -1,7 +1,11 @@
 console.log("Ejecutando JS...");
 
-//----- Obtener elemento de video y configurarlo
-const mainv = document.getElementById('mainv')
+
+const prueba = document.getElementById('pruebas');
+//----- Obtener elemento de video principal
+const mainv = document.getElementById('mainv');
+//----- Obtener elemento de imagen
+
 
 mainv.width=600;  //-- Tamaño de la pantalla de video main
 mainv.height=300;
@@ -12,7 +16,8 @@ for (var i = 0; i < videos.length; i++) {
   videos[i].height = 100;
 }
 
-function main (video) {
+function main (video, img) {
+  prueba.style = "border: 10px solid black; width: 22.5%";
   for (var i = 0; i < videos.length; i++) {
     videos[i].play();
     videos[i].muted=true;
@@ -25,5 +30,14 @@ function main (video) {
     mainv.src = video.src;
     mainv.currentTime = video.currentTime;
     mainv.play();
-
   }
+
+function pruebas (img) {
+  for (var i = 0; i < videos.length; i++) {
+    videos[i].style = "border: 10px solid black; width: 22.5%";
+   }
+   img.style = "border: 10px solid #00FF00; width: 22.5%";
+   console.log("pruebas");
+   mainv.src = null;
+   mainv.poster = img.src;
+}
